cab1f3fe25b950547e78423ae9e66b19b5effa6f
Joanadayse <daysejoana3@gmail.com>
2022-08-04 21:41:37 -0300

    feito

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,26 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = prompt ("Escreva aqui sua nacionalidade").toLocaleLowerCase ()
+
+
+
+if (nacionalidade === "brasileira" ||  "brasileiro"){
+    console.log("sua nacionalicionalidade é" , nacionalidade)
+
+} else if (nacionalidade ==="argentina" || "argentino") {
+    console.log("Sua nacionalidade é", nacionalidade)
+
+} else if (nacionalidade === "uruguaia" || "uruguaio") {
+    console.log("Sua nacionalidade é", nacionalidade);
+
+} else if (nacionalidade === "chilena" || "chileno") {
+    console.log("Sua nacionalidade é", nacionalidade);
+
+} else if (nacionalidade === "colombiana" || "colombiano") {
+    console.log("Sua nacionalidade é", nacionalidade);
+
+} else {
+     console.log("nacionalidade não encontrada");
+}
+
+
+
+
